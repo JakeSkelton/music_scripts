95c848d9d785ec76456ca5e4c2ea41dbe4a219ff
Jake Skelton <s2571161@ed.ac.uk>
2024-12-30 23:28:47 +0000

    Added fuzzy track title matching

--- discogs_id3_completer.py
+++ discogs_id3_completer.py
@@ -5,13 +5,40 @@ Author: Jake Skelton
 Date Modified: Mon Dec 30 2024
 Copyright (c): 2024 Jake Skelton
 '''
+import numpy as np
 import pandas as pd
 import discogs_client as dc
 import time
+from difflib import SequenceMatcher
 
 
-songs = pd.read_csv('../songs.csv', delimiter=',', on_bad_lines='warn')
-noalbum = songs[pd.isna(songs.Album) * pd.notna(songs.TrackTitle)]
+def FuzzyMatch(s1: str, s2: str, threshold: float = 0.8) -> float:
+    return (SequenceMatcher(None, s1, s2).ratio() > threshold)
+
+
+def GetTrackNum(tracklist: list[dc.models.Track], tracktitle: str) -> int:
+    for i in range(len(tracklist)):
+        # TODO: Fuzzy match?
+        if FuzzyMatch(tracklist[i].title, tracktitle):
+            return i + 1
+    raise ValueError("Input track title not found in tracklist")
+
+
+infile = '../songs_with_ids.csv'
+
+songs = pd.read_csv(infile, delimiter=',', index_col=0, on_bad_lines='warn',
+                    dtype={'TrackArtist':'string',
+                           'TrackTitle':'string',
+                           'Album':'string',
+                           'AlbumArtist':'string',
+                           'TrackNum':'string',
+                           'Year':'Int64',
+                           'Genre':'string',
+                           'Location':'string',
+                           'ID':'Int64'})
+hasid = songs[pd.notna(songs.ID)]
+newentries = hasid.copy()
+# TODO: Parse tracknums. Something like 3/4 should -> 3
 
 with open('token.txt', 'r', newline='\n') as tokenfile:
     token = tokenfile.readline().rstrip()
@@ -19,28 +46,23 @@ with open('token.txt', 'r', newline='\n') as tokenfile:
 cli = dc.Client('JakeSearch', user_token=token)
 cli.backoff_enabled = True
 
-idxs = noalbum.index
+idxs = hasid.index
 for idx in idxs:
-    entry = songs.loc[idx]
-    res = cli.search(track=entry.TrackTitle,
-                     artist=entry.TrackArtist,
-                     type='master').page(1)
-    numresults = len(res)
-    if numresults:
-        first = res[0].main_release
-        # Album
-        album = first.title
-        albumartist = first.artists[0].name  # TODO: Right choice?
-        songs.loc[idx, ['Album', 'AlbumArtist']] = [album, albumartist]
-        # Year (in none already)
-        if pd.isna(entry.Year):
-            songs.loc[idx, 'Year'] = first.year
-        # Genre
-    else:
-        album = '-'
+    print("\r%4d/%-4d"%(idx, len(songs)), end='')
 
-    # TODO: Add progress counter
-    print("%4d/%-4d %-50s : %-s"%(
-        idx, len(songs), entry.TrackArtist + ' - ' + entry.TrackTitle, album))
+    entry = songs.loc[idx]
+    hit = cli.release(entry.ID)
+
+    try:
+        tracknum = str(GetTrackNum(hit.tracklist, entry.TrackTitle))
+    except ValueError as err:
+        print(err)
+        # Can't match track title to release tracklist, skip
+        newentries.loc[idx, 'ID'] = np.nan
+        continue
+
+    newentries.loc[
+        idx, ['Album', 'AlbumArtist', 'TrackNum', 'Year', 'Genre']] = \
+        [hit.title, hit.artists[0].name, tracknum, hit.year, hit.genres[0]]
 
     time.sleep(2)
